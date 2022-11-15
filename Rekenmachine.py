7a2b52458fe765525aca54568c1908ae27cda1b7
HedwigStella <hedwiglejeune@gmail.com>
2022-11-15 14:12:07 +0100

    reken machine final product
    de reken machine werkt nu

--- Rekenmachine.py
+++ Rekenmachine.py
@@ -1,38 +1,44 @@
-def add(x, y):
-    return x + y 
+def plus(x, y):
+    return x + y
 
-def substract (x, y):
-    return x - y 
+def min(x, y):
+    return x - y
 
-def multiply (x, y):
+def vermenigvuldigen(x, y):
     return x * y
 
-def divide (x, y):
-    return x / y 
+def delen(x, y):
+    return x / y
 
-print ("wat wil je doen")
-print (".add")
-print (".substract")
-print ("multiply")
-print ("divide")
 
-keuze = input ("vul uw keuze in (1/2/3/4): ")
-if keuze in ('1' , '2' , '3' , '4'):
-    nr1 = float(input("vul uw eerste nummer in: "))
-     nr2 = float(input("vul uw tweede nummer in: "))
+print("Maak uw keuze")
+print("1.plus")
+print("2.min")
+print("3.vermenigvuldigen")
+print("4.delen")
 
-    if keuze == '1' :
-        print(nr1 , "+" , nr2 , "=" , add(nr1 , nr2))
-    elif keuze == '2' :
-        print(nr1 , "-" , nr2 , "=" , substract(nr1 , nr2))
-     elif keuze == '3' :
-        print(nr1 , "*" , nr2 , "=" , multiply(nr1 , nr2))
-      elif keuze == '4' :
-        print(nr1 , "/" , nr2 , "=" , divide(nr1 , nr2))
+while True:
+    keuze = input("Vul uw keuze in(1/2/3/4): ")
 
-    volgende_berekening = input ("wil je nog een berekening doen (ja/nee): ")
-    if volgende_berekening == "nee"
-        break
+    if keuze in ('1', '2', '3', '4'):
+        num1 = float(input("Voer uw eerste nummer in: "))
+        num2 = float(input("Voer uw tweede nummer in: "))
 
-    else : 
-        print ("ongeldige invoer")
+        if keuze == '1':
+            print(num1, "+", num2, "=", plus(num1, num2))
+
+        elif keuze == '2':
+            print(num1, "-", num2, "=", min(num1, num2))
+
+        elif keuze == '3':
+            print(num1, "*", num2, "=", vermenigvuldigen(num1, num2))
+
+        elif keuze == '4':
+            print(num1, "/", num2, "=", delen(num1, num2))
+    
+        volgende_berekening = input("Wil je nog een berekening doen? (ja/nee): ")
+        if volgende_berekening == "nee":
+          break
+    
+    else:
+        print("Invalid Input")
