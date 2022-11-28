fbd4354086cd0bc6cdf005e33fc655df4eaebaf7
HedwigStella <hedwiglejeune@gmail.com>
2022-11-28 14:27:40 +0100

    dobbelsteen
    Midden in het proces NOG NIET AF

---
+++ Dobbelsteen.py
@@ -0,0 +1,10 @@
+hoeveel_dobbelstenen = input("Hoeveel dobbelstenen wilt u gooien? [1-5]")
+if hoeveel_dobbelstenen == "1":
+    print("Er wordt een dobbelsteen gegooid...")
+
+elif
+    hoeveel_dobbelstenen == "2"
+    print("Er worden twee dobbelstenen gegooid...")
+
+
+    
